5a9af826a35de4cf4fb092533a6fd8aa8a97bed7
William Matkaselkä <william@William-Laptop>
2024-05-22 14:32:54 +0200

    fix lint issues for CardHand

--- src/Entity/CardHand.php
+++ src/Entity/CardHand.php
@@ -6,23 +6,39 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 class CardHand
 {
-    private $cards;
+    /**
+     * @var ArrayCollection<int, Card>
+     */
+    private ArrayCollection $cards;
 
     public function __construct()
     {
         $this->cards = new ArrayCollection();
     }
 
+    /**
+     * Adds a card to the hand.
+     * @param Card $card The card to add.
+     * @return void
+     */
     public function addCard(Card $card)
     {
         $this->cards->add($card);
     }
 
+    /**
+     * Gets the collection of cards.
+     * @return ArrayCollection<int, Card> The collection of cards.
+     */
     public function getCards(): ArrayCollection
     {
         return $this->cards;
     }
 
+    /**
+     * Calculates the total value of the hand, treating Aces as either 1 or 14 to stay under 21.
+     * @return int The total value of the hand.
+     */
     public function getValue(): int
     {
         $value = 0;
@@ -30,7 +46,7 @@ class CardHand
 
         foreach ($this->cards as $card) {
             if (is_numeric($card->getValue())) {
-                $value += $card->getValue();
+                $value += (int) $card->getValue();
             } elseif ($card->getValue() === 'Ace') {
                 $aceCount++;
                 $value += 1;
